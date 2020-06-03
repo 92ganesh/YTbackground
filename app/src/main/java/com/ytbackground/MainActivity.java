d4791fd52a0c3539ccb6f4fe617df24950f64d58
ganesh <ganesh.patra@somaiya.edu>
2020-06-03 18:39:25 +0530

    added user device model analytics

--- app/src/main/java/com/ytbackground/MainActivity.java
+++ app/src/main/java/com/ytbackground/MainActivity.java
@@ -4,17 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     MyWebView webView;
@@ -35,6 +49,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }
+
+        SharedPreferences sharedpreferences = getSharedPreferences("YTB", Context.MODE_PRIVATE);
+        String deviceModel = sharedpreferences.getString("phone","not_available");
+        if(deviceModel==null || deviceModel.equals("not_available")){
+            registerNewDevice(sharedpreferences);
+        }else{
+            final FirebaseDatabase database = FirebaseDatabase.getInstance();
+            final DatabaseReference totalUsers = database.getReference("totalUsers");
+            DatabaseReference userDetails = database.getReference("userDetails");
+            String[] accessedTime = getTime();
+            userDetails.child(deviceModel+"").child(accessedTime[0]).setValue(accessedTime[1]);
+        }
+
+
 
         path = "https://www.youtube.com";
         webView = (MyWebView)findViewById(R.id.webView);
@@ -65,6 +93,82 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    private static void registerNewDevice(final SharedPreferences sharedpreferences){
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference totalUsers = database.getReference("totalUsers");
+        totalUsers.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Long userID = dataSnapshot.getValue(Long.class) ;
+                if(userID!=null) {
+                    userID++;
+                    String userIDandModel =  userID + "_" + getDeviceName();
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    editor.putString("phone", userIDandModel);
+                    editor.commit();
+
+                    totalUsers.setValue(userID);
+                    DatabaseReference userDetails = database.getReference("userDetails");
+                    String[] accessedTime = getTime();
+                    userDetails.child(userIDandModel+"").child(accessedTime[0]).setValue(accessedTime[1]);
+                    Log.d("debug-ac","userIDandModel "+userID+" "+accessedTime[1]);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("debug-ac", "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
+    }
+
+    private static String[] getTime(){
+        Calendar calendar = Calendar.getInstance();
+        Date currentTime = Calendar.getInstance().getTime();
+        String[] time = new String[2];
+        time[0] =  calendar.getTime().getTime()+"";
+        time[1] =  calendar.getTime().toLocaleString();
+        return time;
+    }
+
+    private static String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        }
+        return capitalize(manufacturer) + " " + model;
+    }
+
+    private static String capitalize(String str) {
+        if (TextUtils.isEmpty(str)) {
+            return str;
+        }
+        char[] arr = str.toCharArray();
+        boolean capitalizeNext = true;
+
+        StringBuilder phrase = new StringBuilder();
+        for (char c : arr) {
+            if (capitalizeNext && Character.isLetter(c)) {
+                phrase.append(Character.toUpperCase(c));
+                capitalizeNext = false;
+                continue;
+            } else if (Character.isWhitespace(c)) {
+                capitalizeNext = true;
+            }
+            phrase.append(c);
+        }
+
+        return phrase.toString();
     }
 
     @Override
